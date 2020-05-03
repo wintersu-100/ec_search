cae0b205cc1d1830263f1d5a3970aae72f16ee0e
wintersu-100 <58355407+wintersu-100@users.noreply.github.com>
2020-05-04 00:55:37 +0900

    simplify

--- practice/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Contact from './contact/contact.js/Contact';
-
-ReactDOM.render(React.createElement(Contact, null), document.getElementById('root'));
--- practice/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Contact from './contact/contact';
-
-ReactDOM.render(<Contact />, document.getElementById('root'));
